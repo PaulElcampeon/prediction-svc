74c7ceb45239057a66210e3514f8c2e5a8d06d9e
PaulO <pauloladele@live.co.uk>
2025-04-24 10:28:33 +0100

    Initial commit

---
+++ src/main/java/com/splash/prediction_svc/PredictionSvcApplication.java
@@ -0,0 +1,13 @@
+package com.splash.prediction_svc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PredictionSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PredictionSvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/splash/prediction_svc/PredictionSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.splash.prediction_svc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PredictionSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
